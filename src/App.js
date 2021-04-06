3216433b4fbd5d7b61a483900d49f330351049e5
Nabajyoti <sirnaba@gmail.com>
2021-04-06 19:38:29 +0530

    pages users and new places added, Routing is added useing react router for the new pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+//Pages
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users></Users>
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace></NewPlace>
+        </Route>
+        <Redirect to="/"></Redirect>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
