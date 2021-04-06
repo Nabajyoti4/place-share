9e9ce7e243945f62482cd137b4f170520bf72ed9
Nabajyoti <sirnaba@gmail.com>
2021-04-06 20:51:17 +0530

    Side bar for mobile added

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -4,22 +4,30 @@ import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
-function MainNavigation(props) {
+function MainNavigation() {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Place</Link>
-      </h1>
-      <nav>
-        <NavLinks></NavLinks>
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks></NavLinks>
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Place</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks></NavLinks>
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 }
 
